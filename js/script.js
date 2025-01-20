0a81372a140380c7d546204f906cc2715b0bfa4d
WKHMzl <workhousemzl@gmail.com>
2025-01-20 17:03:04 -0300

    V1.5

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Função de feedback no botão de CTA
     const ctaButton = document.querySelector('.cta-button');
+    
+    // Função de hover no CTA
     ctaButton.addEventListener('mouseenter', () => {
         ctaButton.style.transform = 'scale(1.05)';
     });
-
+    
     ctaButton.addEventListener('mouseleave', () => {
         ctaButton.style.transform = 'scale(1)';
     });
 
-    // Função para rolar suavemente até a seção de compras
+    // Rolagem suave até a seção de compra
     ctaButton.addEventListener('click', (e) => {
         e.preventDefault();
         document.querySelector('#comprar').scrollIntoView({ behavior: 'smooth' });
